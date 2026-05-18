9307bd16badf014f80350e806ce00a64c975a414
Samuel Z. <65832351+lettuceinsalad@users.noreply.github.com>
2026-05-18 16:12:04 -0400

    implement db schemas

---
+++ src/db/index.js
@@ -0,0 +1,9 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
+import postgres from 'postgres';
+import { drizzle } from 'drizzle-orm/postgres-js';
+
+const client = postgres(process.env.DATABASE_URL);
+
+export const db = drizzle(client);
